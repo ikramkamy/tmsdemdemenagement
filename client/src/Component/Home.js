2063826115cdbc0a571cad53447a6b4cc29dbec1
ikram kamy <ikramzerkout13@gmail.com>
2022-12-30 17:58:33 +0100

    30/12/2022 third

--- client/src/Component/Home.js
+++ client/src/Component/Home.js
@@ -1,21 +1,13 @@
 import React, { useState } from 'react'
 import './Home.css'
-import Pro from '../images/icons/1.png'
-import Etudiant from '../images/icons/2.png'
-import Camion from '../images/icons/3.png'
-import Avion from '../images/icons/4.png'
-import PlanificationTimeLine from '../images/planificationTimeLine.png'
-import Icon_Plan from '../images/icon_Plan de travail 1.png'
 import Trace from '../images/2.png'
-import Step2 from '../images/step2.png'
-import Step3 from '../images/step3.png'
 import Footer from './footer/Footer'
 import Navbar from './navBar/NavBar'
 import {Link} from "react-router-dom";
 import './animationHome.css';
 import {FaArrowRight} from 'react-icons/fa';
 export default function Home() {
-  const [isScrollActive,setIsScrollActive]=useState(false)
+const [isScrollActive,setIsScrollActive]=useState(false)
   
   /*
   const handelscroll=(e)=> {
@@ -121,144 +113,56 @@ vous pouvez nous écrire anytime en remplissant le formulaire ci-dessous*/}
                     </div>
                 </div>
             </div>
+            <div className="textFiled">
+             {/*   <p>
+                    L’envie de déménager résulte dans 95% des cas d'un désir profond de changement
+                    grâce auquel vous pouvez repartir du bon pied à la conquête de nouvelles opportunités pour une vie prospère. 
+             </p>*/}
+    <div className='home_img_text-section'>
+    <div className='text_homwall3'>
+   <div className='wrap_text_homewall3_backgroundopacity'></div>
+    <div className='wrap_text_homewall3'>
+   <h2 className='animation_typing'>Avez-vous besois des équipements de démènagement?<br/></h2>
+   <h4 style={{color:"#2C216F"}}>Découvrez nos produits et faîtes vos achats en ligne.</h4>
+   <Link to="/boutique" className='btn_home_imagetext-section'>Notre Boutique</Link>
+     </div>    
+    </div>
+    <div className='homewall3_wrap'></div>
+    </div>
 
+   
+            </div>
 
-            <div className='large_home_image' >
-<div>
-<h1></h1>
-    
+ <div className='large_home_image' >
+<div className='ruban_home'>
+<h1>Planifiez Votre
+Déménagement Maintenant
+</h1>
+    <div className='steps'>
+        <div className='mstep'>
+            <img src="/images/icones/calculator.png" className='stap_img'/>
+           <p>Choisissez une de nos <Link to="/services">
+            formules</Link> ou calculer votre devis en cliquant  <Link to="/soto"> ici</Link></p> </div>
+        <div className='mstep'>
+        <img src="/images/icones/validation.png" className='stap_img'/>
+        <p>Après avoir validé l’ensemble des informations. cliquez sur <Link to="/services">Envoyer</Link> </p></div>
+        <div className='mstep'>
+        <img src="/images/icones/printer.png" className='stap_img'/>
+           <p>Imprimez votre inventaire, le reste...c'est à nous de le faire 👷‍♂️</p> </div>
+    </div>
 </div>
 
 </div>
 
             <div className="reasonsContainer">
 
-<div className="reasonsContainerTitle">
-    <div className={`${isScrollActive ? "numberReasons animation_one" : "numberReasons"}`} id="animone">
-        3
-    </div>
-    <div className="rowTitleReasonContainer">
-        <h1 className={`${isScrollActive ? "animation_two" : ""}`}> raisons</h1>
-        <h2 className={`${isScrollActive ? "animation_three" : ""}`}> pour nous choisir</h2>
-    </div>
-</div>
 
 
-<div className="reasonContent">
 
-    <div className="reasonItem">
-        <h1> Authenticité </h1>
-        <div className="reasonItemPara"> <p> Nous ne sous traitons pas,
-            nous possédons notre propre équipe
-            de déméngeurs  </p> </div>
-    </div>
 
-    <div className="reasonItem">
-        <h1> Responsabilité </h1>
-        <div className="reasonItemPara"> <p>
-            Vos affaires arriverons chez vous, en toute
-            sécurité et sans aucun oubli. </p> </div>
-    </div>
-
-    <div className="reasonItem troisieme">
-        <h1> Ponctualité</h1>
-        <div className="reasonItemPara"> <p>
-            Nous nous
-            présenterons le jour
-            J chez vous
-            à l’heure convenue </p> </div>
-    </div>
-</div>
 
 </div>
         
-
-
-
-
-
-
-
-
-
-            <div className="planificationStep">
-                <div className="planificationTitle">
-
-                  <div>  <h1> Planifiez Votre</h1>
-                    <h2> Déménagement Maintenant </h2>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-
-            <div className="planificationTimeLine" id="#planificationTimeLine">
-
-                <div className="planificationLeftSide">
-                    <img src={PlanificationTimeLine} />
-
-                    <div className="buttonBox">
-                        <div>  Je déménage  </div>
-                    </div>
-                </div>
-
-
-                <div className="planificationRightSide">
-                    <div className="step">
-
-                        <div className="descriptionPlanification">
-                            <div> Personnalisez en quelques clics votre déménagement</div>
-                        </div>
-
-                        <div className="illustration">
-                            <div> <img src={Icon_Plan} /> </div>
-                        </div>
-
-                    </div>
-
-                    <div className="step">
-
-                        <div className="descriptionPlanification">
-                            <div> Précisez la date et l’heure qui vous conviennent</div>
-                        </div>
-
-                        <div className="illustration">
-                            <div> <img src={Step2} /> </div>
-                        </div>
-
-
-                    </div>
-                    <div className="step">
-
-
-                        <div className="descriptionPlanification text3">
-                            <div>  Jour J : nos déménageurs seront en place pour transporter vos affaires jusqu’à votre nouvelle déstination</div>
-                        </div>
-
-                        <div className="illustration">
-                            <div> <img src={Step3} /> </div>
-                        </div>
-
-
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="buttonAction">
-
-                <div className="button">
-                    <button className="btn-devis btn-homepage"><Link to="/boutique" > Boutique </Link> </button>
-                </div>
-
-                <div className="button">
-                    <button className='btn-devis btn-homepage2'><Link to="/boutique">Location espace stockage </Link> </button>
-                </div>
-
-            </div>
 
 
             <div className="testimonial">
